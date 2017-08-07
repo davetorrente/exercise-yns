7d5281f14783f334271749228b11d22cdac5a754
davetorrente <dave.perez.yns@gmail.com>
2017-08-07 16:13:34 +0800

    Hello World

--- helloworld.php
+++ helloworld.php
@@ -1,2 +1,10 @@
-<?php
-echo "Hello World";
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>html_php-1-1</title>
+</head>
+<body>
+<?php echo "Hello World"?>
+</body>
+</html>
