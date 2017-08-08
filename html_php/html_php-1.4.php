216444a8bf13069804faebcb6cf31e62d824968b
davetorrente <dave.perez.yns@gmail.com>
2017-08-08 18:13:45 +0800

    javascript-2-10

---
+++ html_php/html_php-1.4.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>html_php-1-1</title>
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+</head>
+<body>
+<div class="container">
+    <div class="jumbotron" align="center">
+        <form>
+            <input type="number"
+        </form>
+    </div>
+</div>
+</body>
+</html>
