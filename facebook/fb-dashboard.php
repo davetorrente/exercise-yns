e5d51e358564a4d6ac3d7095ffe42b7e9750789c
davetorrente <dave.torrente@gmail.com>
2017-08-24 18:00:40 +0800

    facebook sdk, css, fb-login, fb-dashboard

---
+++ facebook/fb-dashboard.php
@@ -0,0 +1,265 @@
+<?php
+session_start();
+require_once 'src/Facebook/autoload.php';
+$fb = new Facebook\Facebook([
+    'app_id' => '1965745767041050',
+    'app_secret' => 'cd791ff5bf0a591beb27269d5fa5ab64',
+    'default_graph_version' => 'v2.4',
+]);
+$helper = $fb->getRedirectLoginHelper();
+$permissions = ['email']; // optional
+try {
+    if (isset($_SESSION['facebook_access_token'])) {
+        $accessToken = $_SESSION['facebook_access_token'];
+    } else {
+        $accessToken = $helper->getAccessToken();
+    }
+} catch(Facebook\Exceptions\FacebookResponseException $e) {
+    // When Graph returns an error
+    echo 'Graph returned an error: ' . $e->getMessage();
+    exit;
+} catch(Facebook\Exceptions\FacebookSDKException $e) {
+    // When validation fails or other local issues
+    echo 'Facebook SDK returned an error: ' . $e->getMessage();
+    exit;
+}
+if (isset($accessToken)) {
+    if (isset($_SESSION['facebook_access_token'])) {
+        $fb->setDefaultAccessToken($_SESSION['facebook_access_token']);
+    } else {
+        // getting short-lived access token
+        $_SESSION['facebook_access_token'] = (string) $accessToken;
+        // OAuth 2.0 client handler
+        $oAuth2Client = $fb->getOAuth2Client();
+        // Exchanges a short-lived access token for a long-lived one
+        $longLivedAccessToken = $oAuth2Client->getLongLivedAccessToken($_SESSION['facebook_access_token']);
+        $_SESSION['facebook_access_token'] = (string) $longLivedAccessToken;
+        // setting default access token to be used in script
+        $fb->setDefaultAccessToken($_SESSION['facebook_access_token']);
+    }
+    // redirect the user back to the same page if it has "code" GET variable
+    // getting basic info about user
+    try {
+        $profile_request = $fb->get('/me');
+        $profile = $profile_request->getGraphNode()->asArray();
+    } catch(Facebook\Exceptions\FacebookResponseException $e) {
+        // When Graph returns an error
+        echo 'Graph returned an error: ' . $e->getMessage();
+        session_destroy();
+        // redirecting user back to app login page
+        header("Location: fb-login.php");
+        exit;
+    } catch(Facebook\Exceptions\FacebookSDKException $e) {
+        // When validation fails or other local issues
+        echo 'Facebook SDK returned an error: ' . $e->getMessage();
+        exit;
+    }
+    print_r($profile);
+    // printing $profile array on the screen which holds the basic info about user
+    // Now you can redirect to another page and use the access token from $_SESSION['facebook_access_token']
+} else {
+    // replace your website URL same as added in the developers.facebook.com/apps e.g. if you used http instead of https and you used non-www version or www version of your website then you must add the same here
+    $loginUrl = $helper->getLoginUrl('http://local.exercise.com/facebook/fb-login.php', $permissions);
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Facebook API</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/style.css" rel="stylesheet">
+    <link href="css/font-awesome.css" rel="stylesheet">
+</head>
+<body>
+<section>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h3 class="panel-title"><?php echo isset($_SESSION['fullname']) ? $_SESSION['fullname'] . '\'s' : '';?> Profile</h3>
+                    </div>
+                </div>
+                <div class="panel panel-default post">
+                    <div class="panel-body">
+                        <div class="row">
+                            <div class="col-sm-2">
+                                <a href="profile.html" class="post-avatar thumbnail"><img src="img/user.png" alt=""><div class="text-center">DevUser1</div></a>
+                                <div class="likes text-center">7 Likes</div>
+                            </div>
+                            <div class="col-sm-10">
+                                <div class="bubble">
+                                    <div class="pointer">
+                                        <p>Hey I was wondering if you wanted to go check out the football game later. I heard they are supposed to be really good!</p>
+                                    </div>
+                                    <div class="pointer-border"></div>
+                                </div>
+                                <p class="post-actions"><a href="#">Comment</a> - <a href="#">Like</a> - <a href="#">Follow</a> - <a href="#">Share</a></p>
+                                <div class="comment-form">
+                                    <form class="form-inline">
+                                        <div class="form-group">
+                                            <input type="text" class="form-control" placeholder="enter comment">
+                                        </div>
+                                        <button type="submit" class="btn btn-default">Add</button>
+                                    </form>
+                                </div>
+                                <div class="clearfix"></div>
+
+                                <div class="comments">
+                                    <div class="comment">
+                                        <a href="#" class="comment-avatar pull-left"><img src="img/user.png" alt=""></a>
+                                        <div class="comment-text">
+                                            <p>I am just going to paste in a paragraph, then we will add another clearfix.</p>
+                                        </div>
+                                    </div>
+                                    <div class="clearfix"></div>
+                                    <div class="comment">
+                                        <a href="#" class="comment-avatar pull-left"><img src="img/user.png" alt=""></a>
+                                        <div class="comment-text">
+                                            <p>I am just going to paste in a paragraph, then we will add another clearfix.</p>
+                                        </div>
+                                    </div>
+                                    <div class="clearfix"></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="panel panel-default post">
+                    <div class="panel-body">
+                        <div class="row">
+                            <div class="col-sm-2">
+                                <a href="profile.html" class="post-avatar thumbnail"><img src="img/user.png" alt=""><div class="text-center">DevUser1</div></a>
+                                <div class="likes text-center">7 Likes</div>
+                            </div>
+                            <div class="col-sm-10">
+                                <div class="bubble">
+                                    <div class="pointer">
+                                        <p>Hey I was wondering if you wanted to go check out the football game later. I heard they are supposed to be really good!</p>
+                                    </div>
+                                    <div class="pointer-border"></div>
+                                </div>
+                                <p class="post-actions"><a href="#">Comment</a> - <a href="#">Like</a> - <a href="#">Follow</a> - <a href="#">Share</a></p>
+                                <div class="comment-form">
+                                    <form class="form-inline">
+                                        <div class="form-group">
+                                            <input type="text" class="form-control" placeholder="enter comment">
+                                        </div>
+                                        <button type="submit" class="btn btn-default">Add</button>
+                                    </form>
+                                </div>
+                                <div class="clearfix"></div>
+
+                                <div class="comments">
+                                    <div class="comment">
+                                        <a href="#" class="comment-avatar pull-left"><img src="img/user.png" alt=""></a>
+                                        <div class="comment-text">
+                                            <p>I am just going to paste in a paragraph, then we will add another clearfix.</p>
+                                        </div>
+                                    </div>
+                                    <div class="clearfix"></div>
+                                    <div class="comment">
+                                        <a href="#" class="comment-avatar pull-left"><img src="img/user.png" alt=""></a>
+                                        <div class="comment-text">
+                                            <p>I am just going to paste in a paragraph, then we will add another clearfix.</p>
+                                        </div>
+                                    </div>
+                                    <div class="clearfix"></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="panel panel-default post">
+                    <div class="panel-body">
+                        <div class="row">
+                            <div class="col-sm-2">
+                                <a href="profile.html" class="post-avatar thumbnail"><img src="img/user.png" alt=""><div class="text-center">DevUser1</div></a>
+                                <div class="likes text-center">7 Likes</div>
+                            </div>
+                            <div class="col-sm-10">
+                                <div class="bubble">
+                                    <div class="pointer">
+                                        <p>Hey I was wondering if you wanted to go check out the football game later. I heard they are supposed to be really good!</p>
+                                    </div>
+                                    <div class="pointer-border"></div>
+                                </div>
+                                <p class="post-actions"><a href="#">Comment</a> - <a href="#">Like</a> - <a href="#">Follow</a> - <a href="#">Share</a></p>
+                                <div class="comment-form">
+                                    <form class="form-inline">
+                                        <div class="form-group">
+                                            <input type="text" class="form-control" placeholder="enter comment">
+                                        </div>
+                                        <button type="submit" class="btn btn-default">Add</button>
+                                    </form>
+                                </div>
+                                <div class="clearfix"></div>
+
+                                <div class="comments">
+                                    <div class="comment">
+                                        <a href="#" class="comment-avatar pull-left"><img src="img/user.png" alt=""></a>
+                                        <div class="comment-text">
+                                            <p>I am just going to paste in a paragraph, then we will add another clearfix.</p>
+                                        </div>
+                                    </div>
+                                    <div class="clearfix"></div>
+                                    <div class="comment">
+                                        <a href="#" class="comment-avatar pull-left"><img src="img/user.png" alt=""></a>
+                                        <div class="comment-text">
+                                            <p>I am just going to paste in a paragraph, then we will add another clearfix.</p>
+                                        </div>
+                                    </div>
+                                    <div class="clearfix"></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="panel panel-default friends">
+                    <div class="panel-heading">
+                        <h3 class="panel-title">My Friends</h3>
+                    </div>
+                    <div class="panel-body">
+                        <ul>
+                            <li><a href="profile.html" class="thumbnail"><img src="img/user.png" alt=""></a></li>
+                            <li><a href="profile.html" class="thumbnail"><img src="img/user.png" alt=""></a></li>
+                            <li><a href="profile.html" class="thumbnail"><img src="img/user.png" alt=""></a></li>
+                            <li><a href="profile.html" class="thumbnail"><img src="img/user.png" alt=""></a></li>
+                            <li><a href="profile.html" class="thumbnail"><img src="img/user.png" alt=""></a></li>
+                            <li><a href="profile.html" class="thumbnail"><img src="img/user.png" alt=""></a></li>
+                            <li><a href="profile.html" class="thumbnail"><img src="img/user.png" alt=""></a></li>
+                            <li><a href="profile.html" class="thumbnail"><img src="img/user.png" alt=""></a></li>
+                            <li><a href="profile.html" class="thumbnail"><img src="img/user.png" alt=""></a></li>
+                        </ul>
+                        <div class="clearfix"></div>
+                        <a class="btn btn-primary" href="#">View All Friends</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<!--<footer>-->
+<!--<div class="container">-->
+<!--<p></p>-->
+<!--</div>-->
+<!--</footer>-->
+
+<!-- Bootstrap core JavaScript
+================================================== -->
+<!-- Placed at the end of the document so the pages load faster -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+<script src="js/bootstrap.js"></script>
+</body>
+</html>
---
+++ facebook/fb-login.php
@@ -0,0 +1,98 @@
+<?php
+session_start();
+require_once 'src/Facebook/autoload.php';
+$fb = new Facebook\Facebook([
+    'app_id' => '1965745767041050',
+    'app_secret' => 'cd791ff5bf0a591beb27269d5fa5ab64',
+    'default_graph_version' => 'v2.4',
+]);
+$helper = $fb->getRedirectLoginHelper();
+$permissions = ['email']; // optional
+
+try {
+    if (isset($_SESSION['facebook_access_token'])) {
+        $accessToken = $_SESSION['facebook_access_token'];
+    } else {
+        $accessToken = $helper->getAccessToken();
+    }
+} catch(Facebook\Exceptions\FacebookResponseException $e) {
+    // When Graph returns an error
+    echo 'Graph returned an error: ' . $e->getMessage();
+    exit;
+} catch(Facebook\Exceptions\FacebookSDKException $e) {
+    // When validation fails or other local issues
+    echo 'Facebook SDK returned an error: ' . $e->getMessage();
+    exit;
+}
+if (isset($accessToken)) {
+    if (isset($_SESSION['facebook_access_token'])) {
+        $fb->setDefaultAccessToken($_SESSION['facebook_access_token']);
+    } else {
+        // getting short-lived access token
+        $_SESSION['facebook_access_token'] = (string) $accessToken;
+        // OAuth 2.0 client handler
+        $oAuth2Client = $fb->getOAuth2Client();
+        // Exchanges a short-lived access token for a long-lived one
+        $longLivedAccessToken = $oAuth2Client->getLongLivedAccessToken($_SESSION['facebook_access_token']);
+        $_SESSION['facebook_access_token'] = (string) $longLivedAccessToken;
+        // setting default access token to be used in script
+        $fb->setDefaultAccessToken($_SESSION['facebook_access_token']);
+    }
+    // redirect the user back to the same page if it has "code" GET variable
+
+    // getting basic info about user
+    try {
+        $profile_request = $fb->get('/me?fields=name,first_name,last_name,email');
+        $profile = $profile_request->getGraphNode()->asArray();
+    } catch(Facebook\Exceptions\FacebookResponseException $e) {
+        // When Graph returns an error
+        echo 'Graph returned an error: ' . $e->getMessage();
+        session_destroy();
+        // redirecting user back to app login page
+        header("Location: fb-login.php");
+        exit;
+    } catch(Facebook\Exceptions\FacebookSDKException $e) {
+        // When validation fails or other local issues
+        echo 'Facebook SDK returned an error: ' . $e->getMessage();
+        exit;
+    }
+
+    // printing $profile array on the screen which holds the basic info about user
+    $_SESSION['fullname'] = $profile['first_name'] . ' ' . $profile['last_name'];
+
+    header("Location: fb-dashboard.php");
+
+
+    // Now you can redirect to another page and use the access token from $_SESSION['facebook_access_token']
+} else {
+    // replace your website URL same as added in the developers.facebook.com/apps e.g. if you used http instead of https and you used non-www version or www version of your website then you must add the same here
+    $loginUrl = $helper->getLoginUrl('http://local.exercise.com/facebook/fb-login.php', $permissions);
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>fb-login</title>
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+    <link href="css/fb-login.css" rel="stylesheet">
+</head>
+<body>
+<div class="container mainDiv">
+    <div class="row">
+        <div class="col-md-offset-6 col-md-3">
+            <div class="form-login">
+                <h2>Welcome back.</h2>
+                <div class="wrapper">
+            <span class="group-btn">
+                <a class="btn btn-primary btn-md" href="<?php echo isset($loginUrl) ? $loginUrl : 'javascript:;';?>">Log in with Facebook! <i class="fa fa-sign-in"></i></a>
+            </span>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
