e9c32c4b5e51674ed0f1b03d0f5fc48faa983f92
davetorrente <dave.torrente@gmail.com>
2017-08-24 10:00:04 +0800

    replace user in git with my email

--- microblog/micro-retweet.php
+++ microblog/micro-retweet.php
@@ -16,25 +16,23 @@ $userId = htmlspecialchars($_POST['user_id']);
 if(($_POST['type']=='forRetweet'))
 {
     $tweet = $_POST['tweet'];
-
-        $isRetweet = true;
-        $database->query("UPDATE tweets SET isRetweet = :isRetweet WHERE id='$tweetId' AND user_id='$userId'");
-        $database->bind(':isRetweet',$isRetweet);
-        $database->execute();
-        $database->query("INSERT INTO tweets (tweet, user_id, parent_tweet, created) VALUES(:tweet, :user_id, :parent_tweet, :created)");
-        $database->bind(':tweet',$tweet);
-        $database->bind(':user_id',$user[0]['id']);
-        $database->bind(':parent_tweet', $tweetId);
-        $database->bind(':created',$datetime);
-        $database->execute();
-        if(!empty($database->lastInsertId())){
-            $database->query('SELECT users.username, users.upload, tweets.id, tweets.tweet, tweets.created, tweets.modified FROM users INNER JOIN tweets ON users.id = tweets.user_id WHERE tweets.id = :id' );
-            $database->bind(':id', $database->lastInsertId());
-            $lastTweet = $database->resultset();
-            $message['isRetweet'] = true;
-            echo json_encode(array('message' => $message, 'query'=>$lastTweet));
-        }
-
+    $isRetweet = true;
+    $database->query("UPDATE tweets SET isRetweet = :isRetweet WHERE id='$tweetId' AND user_id='$userId'");
+    $database->bind(':isRetweet',$isRetweet);
+    $database->execute();
+    $database->query("INSERT INTO tweets (tweet, user_id, parent_tweet, created) VALUES(:tweet, :user_id, :parent_tweet, :created)");
+    $database->bind(':tweet',$tweet);
+    $database->bind(':user_id',$user[0]['id']);
+    $database->bind(':parent_tweet', $tweetId);
+    $database->bind(':created',$datetime);
+    $database->execute();
+    if(!empty($database->lastInsertId())){
+        $database->query('SELECT users.username, users.upload, tweets.id, tweets.tweet, tweets.created, tweets.modified FROM users INNER JOIN tweets ON users.id = tweets.user_id WHERE tweets.id = :id' );
+        $database->bind(':id', $database->lastInsertId());
+        $lastTweet = $database->resultset();
+        $message['isRetweet'] = true;
+        echo json_encode(array('message' => $message, 'query'=>$lastTweet));
+    }
 }
 if(($_POST['type']=='forDelete'))
 {
@@ -55,7 +53,6 @@ if(($_POST['type']=='forDelete'))
         $database->execute();
         $message['success'] = true;
         echo json_encode(array('message'=>$message, 'userID' => $sessionUserID, 'findTweet' => $findRetweet));
-
     }
 
 }
