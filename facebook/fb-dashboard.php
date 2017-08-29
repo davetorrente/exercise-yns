7239a150c85985f697f69dc6fc243b87347e9af1
davetorrente <dave.torrente@gmail.com>
2017-08-29 18:50:50 +0800

    delete js

--- facebook/fb-dashboard.php
+++ facebook/fb-dashboard.php
@@ -220,6 +220,5 @@ ob_end_flush();
 </footer>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
 <script src="js/bootstrap.js"></script>
-<script src="js/main.js"></script>
 </body>
 </html>
--- facebook/js/main.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function(){
-    $('#friends').on('click',function(e){
-        e.preventDefault();
-        alert(1);
-    });
-});
